e444d115a7a7b93beeb085d6b94baf141a5aca83
Lea Rosema <lea.rosema@sinnerschrader.com>
2019-04-20 23:07:46 +0200

    chore: encapsulate controls into x-app

--- public/components/app.js
+++ public/components/app.js
@@ -1,19 +1,25 @@
 export default class App extends HTMLElement {
-  
   static register() {
-    customElements.define('x-app', App);
+    customElements.define("x-app", App);
   }
 
   constructor() {
     super();
-    this.attachShadow({mode: 'open'});
+    this.attachShadow({ mode: "open" });
     this.render();
   }
 
   render() {
     this.shadowRoot.innerHTML = `
       <link rel="stylesheet" href="./components/app.css" />
-      <x-osc-control type="square"></x-osc-control>
-    `
+      <header>
+        <h1>Web Audio Components</h1>
+      </header>
+      <main>
+        <x-osc-control id="osc1" type="sine" freq="55" connect="gain1" start></x-osc-control>
+        <x-gain-control id="gain1" value="0.1" connect="dest"></x-gain-control>
+        <x-destination id="dest"></x-destination>
+      </main>
+    `;
   }
-} 
+}
